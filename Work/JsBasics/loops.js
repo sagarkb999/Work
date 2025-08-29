2c0b0d6e9b184f0d9de7a6863eb373658ff6520e
Sagar Kumar Behera <sagarkumarbehera002@gmail.com>
2025-08-29 12:21:22 +0530

    29.08.25 functions, events

--- Work/JsBasics/loops.js
+++ Work/JsBasics/loops.js
@@ -1,11 +1,28 @@
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
-
-function lCats(cat){
-    return cat.startsWith("L")
+// map() to do something to each item in a collection and create a new collection containing the changed items
+function toUpper(string) {
+  return string.toUpperCase();
 }
 
-const cat= cats.filter(lCats)
-// console.log(cat)
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
+// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
+
+
+// filter() to test each item in a collection, and create a new collection containing only items that match
+function lCat(cat) {
+  return cat.startsWith("L");
+}
+
+const cat = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+const filtered = cat.filter(lCat);
+
+console.log(filtered);
+// [ "Leopard", "Lion" ]
+
 
 const contacts = [
   "Chris:2232322",
@@ -14,35 +31,56 @@ const contacts = [
   "Mary:9998769",
   "Dianne:9384975",
 ];
-for(const cont of contacts){
-const contactSplit= cont.split(":")
-}
-// console.log(contactSplit)
-    for(const i of contactSplit){
-        if (i[0] === "Sarah"){
-            console.log(`Sarah's number is `, i)
-        }
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const searchName = input.value.toLowerCase();
+  for (const contact of contacts) {
+    const splitContact = contact.split(":");
+    if (splitContact[0].toLowerCase() === searchName) {
+      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+      break;
     }
-
-
-
-
-    const output = document.querySelector(".output");
-output.textContent = "";
-
-let i = 10;
-
-while (i >= 0) {
-  const para = document.createElement("p");
-  if (i === 10) {
-    para.textContent = `Countdown ${i}`;
-  } else if (i === 0) {
-    para.textContent = "Blast off!";
-  } else {
-    para.textContent = i;
   }
+  if (para.textContent === "") {
+    para.textContent = "Contact not found.";
+  }
+});
 
-  output.appendChild(para);
+// Challenges
+// Task 2
+// const myArray = ["tomatoes", "chick peas", "onions", "rice", "black beans"];
+// const list = document.createElement("ul");
+// const section = document.querySelector("section");
+// section.appendChild(list);
 
-  i--;
+// for (let arr of myArray){
+//     const item= document.createElement("li")
+//     item.textContent= arr
+//     list.appendChild(item)
+// }
+
+
+// Task 3
+let i = 500;
+const par = document.createElement("p");
+const section = document.querySelector("section");
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
+do {
+  if (isPrime(i)) {
+    para.textContent += `${i}, `;
+  }
+  i--;
+} while (i > 1);
+
+section.appendChild(para);
