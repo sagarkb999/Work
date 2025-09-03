a023b7dc0632ae7fb7fa37713bad77aa3f5f3eb1
Sagar Kumar <sagarkumarbehera002@gmail.com>
2025-09-03 20:07:49 +0530

    abc

---
+++ task4/main.js
@@ -0,0 +1,10 @@
+// COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+function randomValueFromArray() {}
+
+//  RAW TEXT STRINGS
+
+// Event Listeners & Partial function definition
